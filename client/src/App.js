99147d162f2033e7bedcac621638bc6c4d87aa22
Jake Simpson <simpson.jacob.barry@gmail.com>
2019-10-22 11:42:23 -0400

    Section 7 Lecture 39: Alert Components & Action Call

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
 // Redux
 import { Provider } from 'react-redux'; // "connects" redux to react
 import store from './store';
@@ -17,6 +18,8 @@ const App = () => (
         <Navbar />
         <Route exact path='/' component={Landing} />
         <section className='container'>
+          {/* Alert needs to be modified so that it doesn't cause the whole body to shift when it appears */}
+          <Alert />
           <Switch>
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />
